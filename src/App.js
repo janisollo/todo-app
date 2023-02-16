29ea5900f9508185424c3cdf60cc9f8bfd0e371f
janisollo <sollo.jani@gmail.com>
2023-02-16 19:28:33 +0200

    everything

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+import { useState, useEffect } from "react";
+
+const API_BASE = "http://localhost:3001";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+
+    console.log(todos);
+  }, []);
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+        console.log(data); // add this line
+      })
+      .catch((err) => console.error("Error:", err));
+  };
+
   return (
     <div className="App">
       <h1>Welcome Jani</h1>
       <h4>Your Tasks</h4>
 
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox"></div>
-          <div className="text">Get the bread</div>
-          <div className="delete-todo">x </div>
-        </div>
+        {todos.map((todo) => (
+          <div className="todo" key={todo._id}>
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div className="delete-todo">x</div>
+          </div>
+        ))}
       </div>
     </div>
   );

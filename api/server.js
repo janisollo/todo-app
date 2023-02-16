c7f63023e38fd8906a0ff7d8b492cca060c34b63
janisollo <sollo.jani@gmail.com>
2023-02-16 16:57:40 +0200

    Added comments to code with chargpt

--- api/server.js
+++ api/server.js
@@ -1,36 +1,51 @@
+// Import required modules
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+// Create a new Express app
 const app = express();
 
+// Enable JSON parsing middleware for incoming requests
 app.use(express.json());
+
+// Enable CORS middleware to allow cross-origin requests
 app.use(cors());
 
+// Connect to MongoDB using Mongoose
 mongoose
   .connect("mongodb://127.0.0.1:27017/mern-todo", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("connectered to db"))
+  .then(() => console.log("Connected to database"))
   .catch(console.error);
 
+// Import the Todo model
 const Todo = require("./models/Todo");
 
+// Define a route to retrieve all todos from the database
 app.get("/todos", async (req, res) => {
+  // Find all todos using the Todo model
   const todos = await Todo.find();
 
+  // Send the todos as a JSON response
   res.json(todos);
 });
 
-app.get("/todo/new", (req, res) => {
+// Define a route to create a new todo in the database
+app.post("/todo/new", (req, res) => {
+  // Create a new Todo object with the text from the request body
   const todo = new Todo({
     text: req.body.text,
   });
 
+  // Save the new todo to the database
   todo.save();
 
+  // Send the new todo as a JSON response
   res.json(todo);
 });
 
-app.listen(3001, () => console.log("server started on port 3001"));
+// Start the server and listen for incoming requests
+app.listen(3001, () => console.log("Server started on port 3001"));

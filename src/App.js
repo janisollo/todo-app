cebc20baa06899cc1ddca96943d89ff5e9b966e9
janisollo <sollo.jani@gmail.com>
2023-02-17 17:48:17 +0200

    todo app done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
-import { useState, useEffect } from "react";
-
-const API_BASE = "http://localhost:3001";
+import { useEffect, useState } from "react";
+const api_base = "http://localhost:3001";
 
 function App() {
-  // Define the state variables for todos, popupActive, and newTodo using the useState hook
   const [todos, setTodos] = useState([]);
   const [popupActive, setPopupActive] = useState(false);
   const [newTodo, setNewTodo] = useState("");
 
-  // Use the useEffect hook to fetch the todos when the component mounts
   useEffect(() => {
-    // Call the GetTodos function
     GetTodos();
-
-    // Log the current value of the todos state variable
-    console.log(todos);
   }, []);
 
-  // Define the GetTodos function to fetch the todos from the API
   const GetTodos = () => {
-    fetch(API_BASE + "/todos")
+    fetch(api_base + "/todos")
       .then((res) => res.json())
-      .then((data) => {
-        // Set the todos state variable to the data returned from the API
-        setTodos(data);
-
-        // Log the data returned from the API
-        console.log(data);
-      })
-      .catch((err) => console.error("Error:", err));
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("Error: ", err));
   };
 
-  // Define the completeTodo function to mark a todo as completed
   const completeTodo = async (id) => {
-    const data = await fetch(API_BASE + "/todo/complete/" + id).then((res) =>
+    const data = await fetch(api_base + "/todo/complete/" + id).then((res) =>
       res.json()
     );
 
-    // Update the todos state variable to mark the completed todo
     setTodos((todos) =>
       todos.map((todo) => {
-        if (todo._id == data._id) {
+        if (todo._id === data._id) {
           todo.complete = data.complete;
         }
 
@@ -49,41 +33,85 @@ function App() {
     );
   };
 
-  // Define the deleteTodo function to delete a todo
-  const deleteTodo = async (id) => {
-    await fetch(API_BASE + "/todo/delete/" + id, {
-      method: "DELETE",
-    });
+  const addTodo = async () => {
+    const data = await fetch(api_base + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json());
 
-    // Update the todos state variable to remove the deleted todo
-    setTodos((todos) => todos.filter((todo) => todo._id !== id));
+    setTodos([...todos, data]);
+
+    setPopupActive(false);
+    setNewTodo("");
   };
 
-  // Render the component
+  const deleteTodo = async (id) => {
+    const data = await fetch(api_base + "/todo/delete/" + id, {
+      method: "DELETE",
+    }).then((res) => res.json());
+
+    setTodos((todos) => todos.filter((todo) => todo._id !== data.result._id));
+  };
+
   return (
     <div className="App">
-      <h1>Welcome Jani</h1>
-      <h4>Your Tasks</h4>
+      <h1>Welcome, Tyler</h1>
+      <h4>Your tasks</h4>
 
       <div className="todos">
-        {todos.map((todo) => (
-          <div
-            // Add the "is-complete" class to the todo element if it is completed
-            className={"todo " + (todo.complete ? "is-complete" : "")}
-            key={todo._id}
-            onClick={() => completeTodo(todo._id)}
-          >
-            <div className="checkbox"></div>
-            <div className="text">{todo.text}</div>
-            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
-              x
+        {todos.length > 0 ? (
+          todos.map((todo) => (
+            <div
+              className={"todo" + (todo.complete ? " is-complete" : "")}
+              key={todo._id}
+              onClick={() => completeTodo(todo._id)}
+            >
+              <div className="checkbox"></div>
+
+              <div className="text">{todo.text}</div>
+
+              <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+                x
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>You currently have no tasks</p>
+        )}
+      </div>
+
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            X
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
 
-// Export the component
 export default App;
